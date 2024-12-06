b1a7def74bcb74674db16960dde8efcb142c5c5c
Adeel Malik <amalik@liquibase.com>
2024-12-06 11:44:18 -0600

    procedure: raise_prices

--- changelog.sql
+++ changelog.sql
@@ -16,3 +16,9 @@ CREATE TABLE test_table2 (test_id INT, test_column INT, PRIMARY KEY (test_id));
 CREATE TABLE test_table3 (test_id INT, test_column INT, PRIMARY KEY (test_id));
 --rollback DROP TABLE test_table3;
 
+--changeset amalik:raise_prices splitStatements:false
+CREATE PROCEDURE raise_prices ( per_cent INT)
+        UPDATE stock SET unit_price = unit_price + (unit_price * (per_cent/100) );
+END PROCEDURE;
+--rollback drop procedure raise_prices;
+
